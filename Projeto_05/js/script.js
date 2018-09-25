06f67ebfe020379cf835326cf6d864771c92b5ce
Daniel Mendes <dmsdaniel11@gmail.com>
2018-09-25 19:58:27 -0300

    Projeto_05: adicionando scroll dinamico no menu

--- Projeto_05/js/script.js
+++ Projeto_05/js/script.js
@@ -6,6 +6,17 @@ $(function() {
         $(this).find('ul').slideToggle();
 
     });
+
+    $('nav a').on('click', function () {
+
+        var href = $(this).attr('href');
+        var offSetTop = $(href).offset().top;
+
+        $('html, body').animate({
+            'scrollTop': offSetTop
+        });
+
+    });
     
 
 });

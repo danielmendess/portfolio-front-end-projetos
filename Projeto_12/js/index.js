62c7d6d4fe398045812578191750278ae4ab9c6a
Daniel Mendes <dmsdaniel11@gmail.com>
2018-11-01 22:39:54 -0300

    Projeto_12: configuração do slider do bootstrap

--- Projeto_12/js/index.js
+++ Projeto_12/js/index.js
@@ -14,3 +14,9 @@ $(function () {
 
 // Get the current year for the copyright
 $('#year').text(new Date().getFullYear());
+
+// Configure Slider
+$('.carousel').carousel({
+    interval: 6000,
+    pause: 'hover'
+});
